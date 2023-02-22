1f8f08a5d90a69d5b3049e277096e6091b37a1f7
Guybrush <giovanni.cappelletti.1973@gmail.com>
2023-02-22 11:35:53 +0100

    lc - vue route

--- front-end/vue-laravel-microservice/src/App.vue
+++ front-end/vue-laravel-microservice/src/App.vue
@@ -1,175 +1,84 @@
-<script >
-
-import axios from 'axios';
-
-const API_URL = 'http://localhost:8000/api/';
-const API_VER = 'v1/';
-const API = API_URL + API_VER;
-
-const EMPTY_NEW_MOVIE = {
-  tags_id: []
-};
-
-export default {
-
-  data() {
-    
-    return {
-
-      movies: [],
-      genres: [],
-      tags: [],
-
-      new_movie: { ...EMPTY_NEW_MOVIE },
-
-      state: {
-        movieForm: false
-      }
-    }
-  },
-  methods: {
-    
-    editMovie(movie) {
-
-      console.log('movie', movie);
-      console.log('new_movie', this.new_movie);
-
-      this.new_movie = { ...movie };
-
-      this.new_movie.tags_id = [];
-      for (let x=0;x<movie.tags.length;x++) {
-
-        const tag = movie.tags[x];
-        this.new_movie.tags_id.push(tag.id);
-      }
-
-      this.state.movieForm = true;
-    },
-    deleteMovie(movie) {
-
-      axios.delete(API + 'movie/delete/' + movie.id)
-           .then(res => {
-
-              const data = res.data;
-              const success = data.success;
-
-              if (success) 
-                this.updateData();
-           })
-           .catch(err => console.log);
-    },
-    submitMovie(e) {
-
-      e.preventDefault();
-      
-      const new_movie = this.new_movie;
-      const actualApi = API + (
-        'id' in new_movie 
-        ? 'movie/update/' + this.new_movie.id
-        : 'movie/store'
-      );
-
-      console.log(new_movie);
-      console.log(actualApi);
-
-      axios.post(actualApi, new_movie)
-           .then(res => {
-
-              const data = res.data;
-              const success = data.success;
-
-              if (success) {
-
-                this.closeForm();
-                this.updateData();
-              }
-           })
-           .catch(err => console.log);
-    },
-    closeForm() {
-
-      this.new_movie = { ...EMPTY_NEW_MOVIE };
-      this.state.movieForm = false;
-    },
-    updateData() {
-
-      axios.get(API + 'movie/all')
-           .then(res => {
-
-              const data = res.data;
-              const success = data.success;
-
-              const response = data.response;
-
-              const movies = response.movies;
-              const genres = response.genres;
-              const tags = response.tags;
-
-              if (success) {
-
-                this.movies = movies;
-                this.genres = genres;
-                this.tags = tags;
-              }
-           })
-           .catch(err => console.log);
-    }
-  },
-  mounted() {
-
-    this.updateData();
-  }
-};
-
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
-  <div>
-    <h1>Movies</h1>
-    <form v-if="state.movieForm">
-      <label for="name">Name</label>
-      <input type="text" name="name" v-model="new_movie.name">
-      <br>
-      <label for="year">Year</label>
-      <input type="number" name="year" v-model="new_movie.year">
-      <br>
-      <label for="cashOut">Cash out</label>
-      <input type="number" name="cashOut" v-model="new_movie.cashOut">
-      <br>
-      <label for="genre">Genre</label>
-      <select name="genre_id" v-model="new_movie.genre_id">
-        <option v-for="genre in genres" :key="genre.id" :value="genre.id">{{ genre.name }}</option>
-      </select>
-      <br>
-      <label>Tags:</label>
-      <br>
-      <div v-for="tag in tags" :key="tag.id">
-        <input type="checkbox" :id="'tag-' + tag.id" :value="tag.id" v-model="new_movie.tags_id">
-        <label :for="'tag-' + tag.id">{{ tag.name }}</label>
-      </div>
+  <header>
+    HEADER
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
 
-      <button @click="closeForm">CANCEL</button>
-      <input type="submit" @click="submitMovie" :value="'id' in new_movie ? 'UPDATE MOVIE: ' + new_movie.id : 'CREATE NEW MOVIE'">
-    </form>
-    <div v-else>
-      <button @click="state.movieForm = true">CREATE NEW MOVIE</button>
-      <ul>
-        <li v-for="movie in movies" :key="movie.id">
-          {{ movie.name }}
-          <br>
-          <button @click="editMovie(movie)">EDIT</button>
-          <button @click="deleteMovie(movie)">DELETE</button>
-          <ul>
-            <li v-for="tag in movie.tags" :key="tag.id">
-              {{ tag.name }}
-            </li>
-          </ul>
-        </li>
-      </ul>
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">CREATE NEW MOVIE</RouterLink>
+        <a href="http://localhost:8000/movie/create">CREATE NEW MOVIE</a>
+      </nav>
     </div>
-  </div>
+  </header>
+
+  <RouterView />
 </template>
 
 <style scoped>
+header {
+  line-height: 1.5;
+  max-height: 100vh;
+}
 
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+nav {
+  width: 100%;
+  font-size: 12px;
+  text-align: center;
+  margin-top: 2rem;
+}
+
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+
+  nav {
+    text-align: left;
+    margin-left: -1rem;
+    font-size: 1rem;
+
+    padding: 1rem 0;
+    margin-top: 1rem;
+  }
+}
 </style>
